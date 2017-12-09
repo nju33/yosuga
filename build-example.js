208dcad117011602dce4799371e887acb2e780ec
nju33 <nju33.ki@gmail.com>
2017-12-10 01:24:27 +0900

    Update docs

--- build-example.js
+++ build-example.js
@@ -1,51 +1,23 @@
-import autoprefixer from 'autoprefixer';
 import Yosuga from '.';
 
 const yosuga = new Yosuga({
-  title: 'Test',
-  base: `${__dirname}/example/styles/`,
-  targets: ['css', 'scss', 'postcss'],
-  postcssPlugins: [
-    autoprefixer({browsers: ['> 3%', 'last 2 versions']})
-  ],
-  accentColor: '#cb1b45',
-  subColor: '#282425',
-  baseColor: '#f3f3f3',
-
+  name: 'Yosuga DEMO',
+  base: `${process.cwd()}/example`,
+  main: 'sass',
+  targets: ['css', 'scss', 'less'],
+  style: {
+    fontSize: '14px',
+  },
   generate: {
-    dir: 'docs'
+    dir: 'docs',
   },
-  router: {
-    base: '/yosuga/'
-  }
 });
 
-yosuga.process([
-  {
-    name: 'button',
-    description: `
-### headline
-
-description...description...
-
-description...description...description...
-description...description...
-    `
-  },
-  {
-    name: 'table',
-    description: 'description...'
-  },
-  {
-    name: 'nav',
-    description: 'description...'
-  },
-  {
-    name: 'card',
-    description: 'description...'
-  }
-]).then(async () => {
-  await yosuga.generate();
-}).catch(err => {
-  console.log(err);
-});
+yosuga
+  .prepare()
+  .then(() => {
+    yosuga.generate('/yosuga/');
+  })
+  .catch(err => {
+    console.log(err);
+  });
