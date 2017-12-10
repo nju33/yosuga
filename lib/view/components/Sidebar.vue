b1090d0a8b9a6135ee921e1a232c2e1b224d57db
nju33 <nju33.ki@gmail.com>
2017-12-10 17:39:31 +0900

    Revert sidebar type

--- lib/view/components/Sidebar.vue
+++ lib/view/components/Sidebar.vue
@@ -80,7 +80,7 @@ export default {
 	mounted() {
 		this.sidebar = new ApocSidebar(this.$refs.box, {
 			container: this.$parent.$el,
-			type: 'push',
+			type: 'lid',
 		})
 
 		const ro = new ResizeObserver((entries, observer) => {
