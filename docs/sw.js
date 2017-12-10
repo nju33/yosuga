c43ea536ec70e4a1ba604390414522730eea1525
nju33 <nju33.ki@gmail.com>
2017-12-10 17:09:40 +0900

    Remove unused dependencies

--- docs/sw.js
+++ docs/sw.js
@@ -8,28 +8,28 @@ const workboxSW = new self.WorkboxSW({
 
 workboxSW.precache([
   {
-    "url": "/yosuga/_nuxt/app.873e8c517bf8f41f52aa.js",
-    "revision": "3c205b90081efb4ebe2959fc125cfb67"
+    "url": "/yosuga/_nuxt/app.1e889b1af169522cc17b.js",
+    "revision": "a2212cb53fbf559132487f5247c27316"
   },
   {
-    "url": "/yosuga/_nuxt/common.99599cbbdb01c347ca4b.js",
-    "revision": "bb2cbad5afde8493baced74237026933"
+    "url": "/yosuga/_nuxt/common.d25cde6fdc5cd49db353.js",
+    "revision": "f96b3842f8e96719f7450b88c8a8428b"
   },
   {
     "url": "/yosuga/_nuxt/layouts/default.ee665b8ad46016d2c5b2.js",
     "revision": "f7229ba76711ac48c672d9387010bcc4"
   },
   {
-    "url": "/yosuga/_nuxt/manifest.91af43e87f4c410ae8c7.js",
-    "revision": "5528aae7bbd66e7cbd572178f3152585"
+    "url": "/yosuga/_nuxt/manifest.f745ccc42e3d03d23514.js",
+    "revision": "97945872316755f74ed161604a3f94cc"
   },
   {
-    "url": "/yosuga/_nuxt/pages/index.9eaa92a540e72168c49f.js",
-    "revision": "099ad3c2f7219cd9fd1691286bc55879"
+    "url": "/yosuga/_nuxt/pages/index.c98b9d2e66d140b3d5bf.js",
+    "revision": "15987e67f2fdaa04c461e488f69ee72e"
   },
   {
-    "url": "/yosuga/_nuxt/pages/sections/_name.882ca47a8f70cf4f347f.js",
-    "revision": "aea27b84c195674f21046177c790b1bf"
+    "url": "/yosuga/_nuxt/pages/sections/_name.ffcd7a3267fbaf84df0a.js",
+    "revision": "aaa1e7193156121a6aab200fb6f37a98"
   }
 ])
 
