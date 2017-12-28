1d1c8082eca091183b3d0c712e88081a885c89b0
nju33 <nju33.ki@gmail.com>
2017-12-28 16:36:59 +0900

    Update

--- docs/sw.js
+++ docs/sw.js
@@ -8,28 +8,28 @@ const workboxSW = new self.WorkboxSW({
 
 workboxSW.precache([
   {
-    "url": "/yosuga/_nuxt/app.325868885aae299ef07c.js",
-    "revision": "f6855702f30d761a613410b049f64a94"
+    "url": "/yosuga/_nuxt/app.0a86a190f4666162300c.js",
+    "revision": "9fc6cd143c25bcd1452525e5571a88aa"
   },
   {
     "url": "/yosuga/_nuxt/common.4155db0029fc15e3d6b5.js",
     "revision": "736a8560c578a1750f76e591be70ac66"
   },
   {
-    "url": "/yosuga/_nuxt/layouts/default.ee665b8ad46016d2c5b2.js",
-    "revision": "f7229ba76711ac48c672d9387010bcc4"
+    "url": "/yosuga/_nuxt/layouts/default.e91abdcfede10cc06aa5.js",
+    "revision": "d2537010040dd3722fc5ac754e2479ae"
   },
   {
-    "url": "/yosuga/_nuxt/manifest.c01cb06c83e0794c4fc9.js",
-    "revision": "e3e9d929f24a88d40501552f9e76a599"
+    "url": "/yosuga/_nuxt/manifest.c70b465e85193b8f28bd.js",
+    "revision": "941f1a39e1b8556af1003033a26add33"
   },
   {
-    "url": "/yosuga/_nuxt/pages/index.04dc8762166cceb030ba.js",
-    "revision": "1302483ff4ca8cb2f6c75c89ba10e4c5"
+    "url": "/yosuga/_nuxt/pages/index.2e6367a091e2d580d9b3.js",
+    "revision": "c98066625092db570cfc6258181945da"
   },
   {
-    "url": "/yosuga/_nuxt/pages/sections/_name.bd41b27350563f705a2f.js",
-    "revision": "76b670e6000f4452098bdf3e30dfc3f8"
+    "url": "/yosuga/_nuxt/pages/sections/_name.ef6b3388a8fc70a60dca.js",
+    "revision": "d85683d827a0853c89b95ce74adf2f1f"
   }
 ])
 
